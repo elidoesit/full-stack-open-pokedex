b83ca9d255e79153a6b96aab14841a5293c23669
Eli Dahms <eli.dahms@LT-ELID-MAC.local>
2024-04-16 20:16:13 -0700

    11.12

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  const response = 'ok';
+  console.log('Response sent:', response); // Log the response sent to the console
+  res.send(response);
+});
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
